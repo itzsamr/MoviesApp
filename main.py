a8be870cbdb59b3fa6d89e1faa3f6280f8e8ab39
itzsamr <samarathiban@gmail.com>
2024-05-09 14:40:35 +0530

    SQL + Python

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
+import pyodbc
+
+server_name = "SAMAR\MSSQLSERVER01"
+database_name = "MoviesDB"
+
+conn_str = (
+    f"Driver={{SQL Server}};"
+    f"Server={server_name};"
+    f"Database={database_name};"
+    f"Trusted_Connection=yes;"
+)
+
+print(conn_str)
+
+
 print("Hello Word!")
