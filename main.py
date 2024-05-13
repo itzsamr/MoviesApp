9da71010b12787f3a4b8309fb2c20bf5cf8c22d9
itzsamr <samarathiban@gmail.com>
2024-05-13 10:18:11 +0530

    commit tabulate

--- main.py
+++ main.py
@@ -11,7 +11,6 @@ conn_str = (
     f"Trusted_Connection=yes;"
 )
 
-
 print(conn_str)
 conn = pyodbc.connect(conn_str)
 cursor = conn.cursor()
@@ -19,30 +18,19 @@ cursor.execute("Select 1")
 print("Database connection is successful 🎊")
 
 
-# Service - Function that talk to DB | Layer that interacts with DB
-
-
-# Encapsulation
 class MovieService:
     def read_movies(self):
         cursor.execute("Select * from Movies")
-        # movies = cursor.fetchall() # Get all data
-        # for movie in movies:
-        #     print(movie)
+        movies_data = [list(row) for row in cursor.fetchall()]
+        headers = ["MovieId", "Title", "Year", "DirectorId"]
+        print(tabulate(movies_data, headers=headers, tablefmt="grid"))
 
-        # Get data one row at a time
-        for row in cursor:
-            print(row)
-
-    # Task 1
-    # Get the data from the user
-    # Clue: Use arguments
     def create_movie(self, movie):
         cursor.execute(
             "INSERT INTO Movies (Title, Year, DirectorId) VALUES (?, ?, ?)",
             (movie.title, movie.year, movie.director_id),
         )
-        conn.commit()  # Permanent storing | If no commit then no data
+        conn.commit()
 
     def update_movie(self, movie, movie_id):
         cursor.execute(
@@ -53,59 +41,18 @@ class MovieService:
             """,
             (movie.title, movie.year, movie.director_id, movie_id),
         )
-        conn.commit()  # Permanent storing | If no commit then no data
+        conn.commit()
 
-    # Task 2
-    # Delete a movie from the db by getting the id from user
     def delete_movie(self, movie_id):
         cursor.execute("Delete from Movies Where MovieId = ?", movie_id)
         conn.commit()
 
 
-class DirectorService:
-    pass
-
-
-class ActorService:
-    pass
-
-
-# Entity / Model
-# Encapsulation - Movie data
 class Movie:
     def __init__(self, title, year, director_id):
         self.title = title
         self.year = year
         self.director_id = director_id
-
-
-class Director:
-    pass
-
-
-class Actor:
-    pass
-
-
-# Main menu
-# 1. Movie Management
-# 2. Director Management
-# 3. Actor Management
-# 4. Exit
-
-
-# Task 3
-# Movie Management Menu
-# 1. Add a Movie
-# 2. View All Movies
-# 3. Update a Movie  (Task 4)
-# 4. Delete a Movie
-# 5. Back
-
-# C - Create
-# R - Read
-# U - Update
-# D - Delete
 
 
 def movie_menu():
@@ -153,7 +100,6 @@ def actor_menu():
     pass
 
 
-# Task 5 - Keep it in loop
 if __name__ == "__main__":
     print("Welcome to the movies app")
 
@@ -178,6 +124,5 @@ if __name__ == "__main__":
         elif choice == 4:
             break
 
-    # Clean up code
     cursor.close()
     conn.close()
